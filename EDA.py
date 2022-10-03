e3394e4ea409c92f1bb601c7fb876c12f8755378
0xbenb <83n8urd3n@gmail.com>
2022-10-03 13:23:42 +0100

    working through EDA

--- EDA.py
+++ EDA.py
@@ -6,6 +6,13 @@ import time
 import random
 from datetime import datetime, timedelta
 from CoreFunctions import *
+from EDAFunctions import *
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.graph_objects import *
+import plotly.io as pio
+from scipy.stats import skew
+pio.renderers.default = "browser"
 direnv.load()
 pd.options.display.max_rows = 10
 pd.options.display.max_columns = 30
@@ -13,33 +20,6 @@ pd.options.display.width = 10000
 
 engine = Create_SQL_Engine()
 conn = Create_SQL_Connection(db_engine=engine)
-
-# 7 DAY universe stationary definition of universe - will improve later
-
-univ = Universe_Definition(top100_ndays_ago=7, db_engine=engine)
-
-# PULL IN FEATURES FOR TESTING
-
-# keep simple to start -> returns and volume
-
-q = DB_Query_Statement(table_name='features', filter_col='feature', filter_col_vals=['volume_1h_usd'])
-volume = DB_Query(q, db_engine=engine)
-
-q = DB_Query_Statement(table_name='rets', filter_col='feature', filter_col_vals=['ret_1h_neutral'])
-rets = DB_Query(q, db_engine=engine)
-
-input_dat = pd.concat([volume, rets], axis=0)
-input_dat = input_dat.pivot(index=['time', 'coin'], columns='feature', values='value').reset_index()
-
-# ultimately will test a range
-q = DB_Query_Statement(table_name='rets', columns=['time', 'coin', 'value'], filter_col='feature',
-                       filter_col_vals=['fwd_ret_6h_neutral'])
-fwd_rets = DB_Query(q, db_engine=engine)
-fwd_rets.rename({'value': 'fwd_ret'}, axis=1, inplace=True)
-# factor in 1h constraint for putting on positions
-
-
-# using trailing 1h returns + volume to build a signal to capture changes in price dP and volume dV
 
 # MARKET PATTERN / OBSERVATION
 # markets often quote this mixture of patterns describing them as bullish/bearish movements
@@ -51,7 +31,66 @@ fwd_rets.rename({'value': 'fwd_ret'}, axis=1, inplace=True)
 
 # can we combine price / volume trends to predict forward returns
 
+# 7 DAY universe stationary definition of universe - will improve later
 
+univ = Universe_Definition(top100_ndays_ago=7, db_engine=engine)
+
+# PULL IN PREDICTORS FOR TESTING
+X = ['volume_1h_usd', 'ret_1h_neutral']
+
+# PREDICTORs
+q = DB_Query_Statement(table_name='features', filter_col='feature', filter_col_vals=[X[0]])
+volume = DB_Query(q, db_engine=engine)
+
+q = DB_Query_Statement(table_name='rets', filter_col='feature', filter_col_vals=[X[1]])
+rets = DB_Query(q, db_engine=engine)
+
+input_dat = pd.concat([volume, rets], axis=0)
+input_dat = input_dat.pivot(index=['time', 'coin'], columns='feature', values='value').reset_index()
+
+# PULL IN RESPONSE FOR TESTING
+Y = ['fwd_ret_6h_neutral']
+q = DB_Query_Statement(table_name='rets', columns=['time', 'coin', 'value'], filter_col='feature',
+                       filter_col_vals=[Y[0]])
+output_dat = DB_Query(q, db_engine=engine)
+output_dat.rename({'value': 'fwd_ret'}, axis=1, inplace=True)
+
+full_dat = input_dat.merge(output_dat, how='left', on=['time', 'coin'])
+# factor in 1h constraint for putting on positions
+
+# using trailing 1h returns + volume to build a signal to capture changes in price dP and volume dV
+
+# Create plotly charts to ultimately build dashboard
+data = full_dat.copy(deep=True)
+
+pred_var = X[1]
+resp_var = Y[0]
+data = data[['time', 'coin', pred_var]]
+
+Number_Observations_Time(data=full_dat[['time', 'coin', pred_var]], var_name=pred_var)
+
+# massive tails in rets i need to clean this
+test_dat = full_dat.sample(100000)
+px.scatter(test_dat, x='volume_1h_usd', y='fwd_ret', marginal_y='histogram', marginal_x='box', trendline='ols',
+           template='plotly_dark')
+
+
+x =[1,2,3,4,5,5,5,10]
+print(skew(x))
+print(skew(x, bias=False))  # option to adjust for statistical bias - need to understand this more
+
+# how do i want to capture changes in price & volume?
+# many ways to categorise i'm going to try a few and do a quick test of which ones seem more promising
+
+# z-score of price and volumes
+# here i'm leaning towards using skew to represent the movement as i want to not only see spikes in volumes
+# i also want to understand if the overall profile is looking skewed vs history compared to other coins
+
+# 1. calculate skew of returns per coin over trailing period
+# 2. this will capture coins with +ve skew (-ve performance) -ve skew (positive performance)
+# 3. coins with big skew with a jump will be prime for reversal
+
+# might be doing too much here could do more simply but good direction of travel
 
 
 
--- EDAFunctions.py
+++ EDAFunctions.py
@@ -6,6 +6,12 @@ import time
 import random
 from datetime import datetime, timedelta
 from CoreFunctions import *
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.graph_objects import *
+import plotly.io as pio
+pio.renderers.default = "browser"
+
 direnv.load()
 pd.options.display.max_rows = 10
 pd.options.display.max_columns = 30
@@ -14,6 +20,35 @@ pd.options.display.width = 10000
 engine = Create_SQL_Engine()
 conn = Create_SQL_Connection(db_engine=engine)
 
-# 7 DAY universe stationary definition of universe - will improve later
+colour_theme = {'background': '#121212', 'tile': '#181818', 'top_gradient': '#404040',
+                'bottom_gradient': '#282828', 'primary_text': '#FFFFFF', 'secondary_text': '#B3B3B3'}
 
-univ = Universe_Definition(top100_ndays_ago=7)
+def Number_Observations_Time(data: pd.DataFrame, var_name):
+    n_obs = data.groupby(['time'])[var_name].count().reset_index()
+
+    config = dict({'scrollZoom': False, 'displaylogo': False, 'displayModeBar': False})
+
+    # https://blog.karenying.com/posts/50-shades-of-dark-mode-gray
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=n_obs['time'], y=n_obs[var_name], fill='tozeroy'))  # override default markers+lines
+
+    fig.update_layout({
+        'paper_bgcolor': colour_theme['background'],
+        'plot_bgcolor': colour_theme['top_gradient'],
+        'font_family': "Courier New",
+        'font_color': colour_theme['primary_text'],
+        'title_text': f'<b>Observations through time: {var_name}',
+        'titlefont': dict(size=30)
+         })
+
+    fig.update_xaxes(tickfont=dict(size=22), showgrid=False, title='time', zeroline=False, tickprefix='')
+    fig.update_yaxes(tickfont=dict(size=22), showgrid=False, title='n observations', zeroline=False, tickprefix='')
+
+    fig.update_yaxes(title_font=dict(size=30))
+    fig.update_xaxes(title_font=dict(size=30)) # only way to get it update the size for now figure out why
+
+    fig.show(config=config)
+
+
+
