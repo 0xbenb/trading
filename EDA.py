9fe3c737b87168f89cc287d374924ccad8476d68
0xbenb <83n8urd3n@gmail.com>
2022-10-04 10:52:20 +0100

    continuing EDA

--- EDA.py
+++ EDA.py
@@ -34,12 +34,26 @@ Y = ['fwd_ret_6h_neutral']
 
 # can we combine price / volume trends to predict forward returns
 
+# how do i want to capture changes in price & volume?
+# many ways to categorise i'm going to try a few and do a quick test of which ones seem more promising
+
+# z-score of price and volumes
+# i'm leaning towards using skew to represent the movement as i want to not only see spikes in volumes
+# i also want to understand if the overall profile is looking skewed vs history compared to other coins
+
+# 1. calculate skew of returns per coin over trailing period
+# 2. this will capture coins with +ve skew (-ve performance) -ve skew (positive performance)
+# 3. coins with big skew with a jump will be prime for reversal
+
+# might be doing too much here could do more simply but good direction of travel
+# two kind of ideas here histkew & relskew & probably a combo is interesting or almost a diffskew something
+
+
 # 7 DAY universe stationary definition of universe - will improve later
 
 univ = Universe_Definition(top100_ndays_ago=7, db_engine=engine)
 
 # PULL IN PREDICTORS FOR TESTING
-
 
 # PREDICTORs
 q = DB_Query_Statement(table_name='features', filter_col='feature', filter_col_vals=[X[0]])
@@ -60,7 +74,6 @@ output_dat.rename({'value': 'fwd_ret'}, axis=1, inplace=True)
 full_dat = input_dat.merge(output_dat, how='left', on=['time', 'coin'])
 full_dat.to_csv('dat/full_dat.csv', index=False)
 full_dat = pd.read_csv('dat/full_dat.csv')
-# factor in 1h constraint for putting on positions
 
 # using trailing 1h returns + volume to build a signal to capture changes in price dP and volume dV
 
@@ -76,94 +89,64 @@ px.scatter(test_dat, x='volume_1h_usd', y='fwd_ret', marginal_y='histogram', mar
            template='plotly_dark')
 
 
-x =[1,2,3,4,5,5,5,10]
-print(skew(x))
-print(skew(x, bias=False))  # option to adjust for statistical bias - need to understand this more
-
-# how do i want to capture changes in price & volume?
-# many ways to categorise i'm going to try a few and do a quick test of which ones seem more promising
-
-# z-score of price and volumes
-# i'm leaning towards using skew to represent the movement as i want to not only see spikes in volumes
-# i also want to understand if the overall profile is looking skewed vs history compared to other coins
-
-# 1. calculate skew of returns per coin over trailing period
-# 2. this will capture coins with +ve skew (-ve performance) -ve skew (positive performance)
-# 3. coins with big skew with a jump will be prime for reversal
-
-# might be doing too much here could do more simply but good direction of travel
-
-# two kind of ideas here histkew & relskew & probably a combo is interesting or almost a diffskew something
-
-# start simple let's just build skew first
-
 full_dat = pd.read_csv('dat/full_dat.csv')
 full_dat = full_dat[['time', 'coin', pred_var]]
-t_period = 14 * 24 #  14 days
+t_period = 7 * 24  # start with 1 week
 
-def Sample_Data(data:pd.DataFrame, column: str, n_sample):
-    subset = np.random.choice(data[column].unique(), n_sample)
-    data = data[data[column].isin(subset)]
-
-    return data
-
-def calc_skew(x):
-    return skew(x, bias=False)  # calculations are corrected for bias is bias=False
-
-sample_dat = Sample_Data(data=full_dat, column='coin', n_sample=5)
-
-res = sample_dat.groupby('coin', group_keys=True)['ret_1h_neutral'].apply(
-    lambda x: x.rolling(5).apply(calc_skew)
-)
-sample_dat['skew'] = res.reset_index(level=0, drop=True)
-
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
-
-z = sample_dat.loc[(full_dat['coin'] == 'VET') & (full_dat['time'] >= '2018-07-25 05:00:00+00:00')]
-
-z['ret_1h_neutral'].iloc[0:5]
-# https://stackoverflow.com/questions/51935456/std-skew-giving-wrong-answer-with-rolling
 
 # pandas calculates unbiased skew i.e. bias = False (scipy defaults to true)
 # If bias = False, the calculations are corrected for bias
 # To make scipy.stats.skew compute the same value as the skew() method in Pandas, add the argument bias=False.
 # scipy calculates biased skew as default i.e. bias = True
 
+full_dat = Calculate_Skew(data=full_dat, variable=pred_var, t_window=t_period, bias=False)
 
-sample_dat['pandas_skew'] = sample_dat.groupby('coin')[pred_var].rolling(5).skew().reset_index(level=0, drop=True)
-# shows that bias = False with pandas i.e. it is unbiased value
+full_dat[f'{pred_var}_skew'] = full_dat.groupby('coin')[pred_var].rolling(t_period).skew().reset_index(level=0, drop=True)
 
-sample_dat['test'] = round(sample_dat['skew'], 6) == round(sample_dat['pandas_skew'], 6) # the same apart from rounding
+full_dat = Standardise(data=full_dat, method='zscore', t_window=t_period, variable=f'{pred_var}_skew')
+
+# PREPARING PREDICTOR(S) VARIABLE(S)
+
+# in terms of treating outliers, don't want to over clean. generally raw data > calculate > standardise > normalise
+# can ultimately test approach to see which option gives the best predictions
+
+# TRAILING RETURNS (CHANGE IN PRICE)
+
+# CLEAN RETURNS
+# REMOVE TOP / BOTTOM 1-2.5%)
+# - normalise
+# - https://stackoverflow.com/questions/29275210/how-to-use-sigmoid-function-to-normalized-a-list-of-vaues
+# MAD
+# SIGMOID -1 & +1
+
+# https://stats.stackexchange.com/questions/7757/data-normalization-and-standardization-in-neural-networks
+# import sklearn
+#
+# # Normalize X, shape (n_samples, n_features)
+# X_norm = sklearn.preprocessing.normalize(X)
+
+# https://stackoverflow.com/questions/43061120/tanh-estimator-normalization-in-python
+
+unnormalizedData = np.array([1,5,3,12,1,4345,4], dtype=np.float64)
+
+m = np.mean(unnormalizedData, axis=0) # array([16.25, 26.25])
+std = np.std(unnormalizedData, axis=0) # array([17.45530005, 22.18529919])
+
+data = 0.5 * (np.tanh(0.01 * ((unnormalizedData - m) / std)) + 1)
+
+np.tanh(data)
+
+full_dat['ret_1h_neutral_tanh'] = np.tan(full_dat[pred_var])
+
+# normalise --> scale
+# i.e. calculate z score
+# apply sigmoid function
+# done
+
+# https://www.geeksforgeeks.org/python-pytorch-tanh-method/
+# https://www.geeksforgeeks.org/python-tensorflow-nn-tanh/
 
 
-
-# next need to think about how to prepare the other input
-# volume 1h usd
-# neutral volume?
-# you can z-score per coin rolling window which will pick up changes i suppose
-# i wonder if doing % volume change (- market volume) is a good way to take this
-
-
-# formulate testing process strategy thoroughly before continuing
-
-# there are a few ways to build signals to capture this
-# there can be more than one working simultaneously to capture it
-# pick a couple to capture dp dV then go build testing process
-
-# - put stuff in neutral terms
-# - do quicktests on signals
-# - pursue accordingly
-# mixture of z-scores and skews can capture a lot of this
-# can test individually but when they combine it might get more interesting
-
-# e.g. skew of returns to map overall profile & z-score of returns to understand how high current return is
-# want to understand how extended is the move
-
-
-# FINISH PREPARING PREDICTOR(S) VARIABLE(S)
-
-# RETURNS (CHANGE IN PRICE)
 #   ZSCORE (ALREADY NEUTRAL)
 #   SKEW
 
@@ -176,6 +159,19 @@ sample_dat['test'] = round(sample_dat['skew'], 6) == round(sample_dat['pandas_sk
 # APPLY SIGMOID FUNCTION
 
 # SPLIT INTO BUCKETS LOOK AT NEUTRAL RET PROFILE
+# DECIDE HOW TO EVOLVE / COMBINE SIGNALS
 
 # BACKTEST
+
+
+
+
+
+
+
+# Loose ends / Reminders
+# # factor in 1h constraint for putting on positions
+
+
+
 
--- EDAFunctions.py
+++ EDAFunctions.py
@@ -6,6 +6,7 @@ import time
 import random
 from datetime import datetime, timedelta
 from CoreFunctions import *
+from scipy.stats import skew, zscore
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.graph_objects import *
@@ -22,6 +23,14 @@ conn = Create_SQL_Connection(db_engine=engine)
 
 colour_theme = {'background': '#121212', 'tile': '#181818', 'top_gradient': '#404040',
                 'bottom_gradient': '#282828', 'primary_text': '#FFFFFF', 'secondary_text': '#B3B3B3'}
+
+
+def Sample_Data(data:pd.DataFrame, column: str, n_sample):
+    subset = np.random.choice(data[column].unique(), n_sample)
+    data = data[data[column].isin(subset)]
+
+    return data
+
 
 def Number_Observations_Time(data: pd.DataFrame, var_name):
     n_obs = data.groupby(['time'])[var_name].count().reset_index()
@@ -49,6 +58,59 @@ def Number_Observations_Time(data: pd.DataFrame, var_name):
     fig.update_xaxes(title_font=dict(size=30)) # only way to get it update the size for now figure out why
 
     fig.show(config=config)
+
+
+def Calculate_Skew(data, variable, t_window, bias: bool):
+
+    # for unbiased data - pandas faster
+    if not bias:
+        data[f'{variable}_skew'] = data.groupby('coin')[variable].rolling(t_window).skew().reset_index(level=0, drop=True)
+
+        return data
+
+    # for biased data
+    if bias:
+        def calc_skew(x):
+            return skew(x, bias=True)  # calculations are corrected for bias is bias=False (scipy defaults to True)
+
+        res = data.groupby('coin', group_keys=True)['ret_1h_neutral'].apply(
+            lambda x: x.rolling(t_window).apply(calc_skew)
+        )
+        data['skew'] = res.reset_index(level=0, drop=True)
+
+        return data
+
+def Standardise(data: pd.DataFrame, method: str, t_window: int, variable: str):
+    """
+
+    :param data: data block of time, coin, variable, ...
+    :param method: zscore
+    :param t_window: time window for calculation
+    :param variable: variable to standardise
+    :return:
+    """
+    if method == 'zscore':
+
+        # (N-ddof) ddof = 0 for entire population (N) or ddof = 1 for sample (N-1) (n big variance sample = pop)
+        data['mean'] = data.groupby('coin')[variable].rolling(t_window).mean().reset_index(level=0, drop=True)
+        data['std'] = data.groupby('coin')[variable].rolling(t_window).std(ddof=0).reset_index(level=0, drop=True)
+        data['zscore'] = (data[variable] - data['mean']) / data['std']
+
+        # z = data.loc[(data['coin'] == 'BTC') & (data['time'] >= '2017-08-28 08:00:00+00:00')]
+        # series = z[variable].iloc[0:5, ]
+        # zscore(series, ddof=0)
+
+        data.drop(['mean', 'std'], axis=1, inplace=True)
+
+    return data
+
+
+
+
+
+
+
+
 
 
 
